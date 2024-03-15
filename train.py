6ea56c182ff0a59835c00f73be2d8b79cecbe587
Aditya Pulipaka <pulipakaa24@outlook.com>
2024-03-15 00:20:20 -0500

    other shi

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
  
 # Load the model.
-model = YOLO('yolov9c.pt')
+model = YOLO('yolov8n.pt')
  
 # Training.
 results = model.train(
    data='tennis.yaml',
    imgsz=1280,
    epochs=50,
-   batch=8,
+   batch=32,
    workers=0,
-   name='yolov9c_8b_50e')
+   name='yolov8n_32b_50e')
